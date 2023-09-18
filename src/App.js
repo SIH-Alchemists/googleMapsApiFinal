1e07e8ce292c38d69ffd08cf8336f1577a6d0ee4
nehapol <nehapol40@gmail.com>
2023-09-18 17:07:32 +0530

    map ka almost ho gaya

--- src/App.js
+++ src/App.js
@@ -3,36 +3,20 @@
 import React,{useMemo} from 'react';
 import Map from './Map'; // Adjust the import path
 import {
-  Autocomplete, useLoadScript
+  useLoadScript
 } from '@react-google-maps/api'
-import { useRef, useState } from 'react'
+import { useState } from 'react'
 function App() {
   // Fetch agency data and calculate nearby agencies
   // Define your agency location (myLat, myLng) and nearbyAgencies array here
 
-  /** @type React.MutableRefObject<HTMLInputElement> */
-  const originRef = useRef('')
+  // /** @type React.MutableRefObject<HTMLInputElement> */
+  // const originRef = useRef('')
 
   
-  function calculateDistance(lat1, lon1, lat2, lon2) {
-    const R = 6371; // Earth's radius in km
-    const dLat = ((lat2 - lat1) * Math.PI) / 180;
-    const dLon = ((lon2 - lon1) * Math.PI) / 180;
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos((lat1 * Math.PI) / 180) *
-        Math.cos((lat2 * Math.PI) / 180) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in km
-    return distance;
-  }
+  
   const myAgencyLocation = { lat: 18.528423, lng: 73.873863,name:"pune station" }; // pune station Replace with your agency's location
-  if (originRef.current.value) {
-    
-    console.log(originRef.current.value);
-  }
+  
   // const agencies = [
   //   {lat: 18.457533, lng: 73.867744,name:"Katraj"},
   //   {lat: 18.499081, lng: 73.934174,name:"hadapsar"},
@@ -40,27 +24,7 @@ function App() {
   //   {lat: 18.526110, lng: 73.844131,name:"shivaji nagar"}
   // ]
 
-  const generateAgencies = (position) => {
-    const _houses = [];
-    for (let i = 0; i < 100; i++) {
-      const direction = Math.random() < 0.5 ? -2 : 2;
-      _houses.push({
-        lat: position.lat + Math.random() / direction,
-        lng: position.lng + Math.random() / direction,
-      });
-    }
-    return _houses;
-  };
-  const newagencies = useMemo(() => generateAgencies(myAgencyLocation), [myAgencyLocation]);
-  const nearbyAgencies = newagencies.filter((agency) => {
-    const distance = calculateDistance(
-      myAgencyLocation.lat,
-      myAgencyLocation.lng,
-      agency.lat,
-      agency.lng
-    );
-    return distance <= 20; // 20km radius
-  });
+  
   // const showAgencies=()=>{
 
   // }
@@ -72,13 +36,7 @@ function App() {
   if (!isLoaded) return <div>Loading...</div>;
   return (
     <div>
-        <Autocomplete>
-              <input type='text' placeholder='Origin' ref={originRef} />
-            </Autocomplete>
-            {/* <button type='submit' onClick={showAgencies}>
-              Calculate Route
-            </button> */}
-      <Map agencies={nearbyAgencies} initialCenter={myAgencyLocation} initialZoom={10} />
+      <Map initialCenter={myAgencyLocation} initialZoom={10} />
     </div>
   );
 }
@@ -88,3 +46,5 @@ export default App;
 
 
 // process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+
+
--- src/Map.js
+++ src/Map.js
@@ -2,20 +2,19 @@ import React, { useState, useRef, useMemo, useCallback } from "react";
 import {
   GoogleMap,
   Circle,
- 
   Marker,
   MarkerClusterer,
- 
   DirectionsRenderer,
 } from "@react-google-maps/api";
 import Distance from "./distance";
-const Map = ({ agencies, initialCenter, initialZoom }) => {
+import "./index.css";
+import Places from "./places";
+const Map = ({ initialCenter, initialZoom }) => {
   const [selectedAgency, setSelectedAgency] = useState(null);
   const [directions, setDirections] = useState(null);
-  // const [duration, setDuration] = useState('')
+  const [currAgency, setCurrAgency] = useState(initialCenter);
   const [directionsResponse, setDirectionsResponse] = useState(null);
-  // const [distance, setDistance] = useState(null);
-
+  const mapRef = useRef();
   const onMarkerClick = (agency) => {
     setSelectedAgency(agency);
     calculateDirections(agency);
@@ -29,7 +28,7 @@ const Map = ({ agencies, initialCenter, initialZoom }) => {
     // eslint-disable-next-line no-undef
     const directionsService = new google.maps.DirectionsService();
     const results = await directionsService.route({
-      origin: initialCenter,
+      origin: currAgency,
       destination: selectedAgency,
       // eslint-disable-next-line no-undef
       travelMode: google.maps.TravelMode.DRIVING,
@@ -37,14 +36,48 @@ const Map = ({ agencies, initialCenter, initialZoom }) => {
     setDirectionsResponse(results);
   }
 
+  function calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371; // Earth's radius in km
+    const dLat = ((lat2 - lat1) * Math.PI) / 180;
+    const dLon = ((lon2 - lon1) * Math.PI) / 180;
+    const a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos((lat1 * Math.PI) / 180) *
+        Math.cos((lat2 * Math.PI) / 180) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c; // Distance in km
+    return distance;
+  }
+  const generateAgencies = (position) => {
+    const _agency = [];
+    for (let i = 0; i < 100; i++) {
+      const direction = Math.random() < 0.5 ? -2 : 2;
+      _agency.push({
+        lat: position.lat + Math.random() / direction,
+        lng: position.lng + Math.random() / direction,
+      });
+    }
+    const agencies = _agency.filter((agency) => {
+      const distance = calculateDistance(
+        currAgency.lat,
+        currAgency.lng,
+        agency.lat,
+        agency.lng
+      );
+      return distance <= 30;
+    });
+    return agencies;
+  };
+ 
+  const agencies = useMemo(() => generateAgencies(currAgency), [currAgency]);
+
   const calculateDirections = (agency) => {
     const directionsService = new window.google.maps.DirectionsService();
     directionsService.route(
       {
-        origin: new window.google.maps.LatLng(
-          initialCenter.lat,
-          initialCenter.lng
-        ),
+        origin: new window.google.maps.LatLng(currAgency.lat, currAgency.lng),
         destination: new window.google.maps.LatLng(agency.lat, agency.lng),
         travelMode: window.google.maps.TravelMode.DRIVING,
       },
@@ -57,7 +90,7 @@ const Map = ({ agencies, initialCenter, initialZoom }) => {
       }
     );
   };
-  const mapRef = useRef();
+
   const onLoad = useCallback((map) => (mapRef.current = map), []);
   const options = useMemo(
     () => ({
@@ -67,11 +100,6 @@ const Map = ({ agencies, initialCenter, initialZoom }) => {
     }),
     []
   );
-
-  const handleResetMap = () => {
-    setSelectedAgency(null);
-    setDirections(null);
-  };
 
   const renderMarkers = () => {
     return agencies.map((agency, index) => (
@@ -85,27 +113,37 @@ const Map = ({ agencies, initialCenter, initialZoom }) => {
   };
 
   return (
-    <div>
-      {directions && <Distance leg={directions.routes[0].legs[0]} />}
-      <button
-        onClick={() => {
-          mapRef.current.panTo(initialCenter);
-          mapRef.current.setZoom(10);
-        }}
-        
-      >
-        Zoom out
-      </button>
-      {/* <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}> */}
+    <div className="container">
+      <div className="controls">
+        <h1>Find nearby agencies</h1>
+        <Places
+          setAgency={(agency) => {
+            setCurrAgency(agency);
+            mapRef.current?.panTo(agency);
+          }}
+        />
+        {!currAgency && <p>Enter the address of your agency.</p>}
+
+        {directions && <Distance leg={directions.routes[0].legs[0]} />}
+        <button
+          onClick={() => {
+            mapRef.current.panTo(currAgency);
+            mapRef.current.setZoom(10);
+          }}
+        >
+          Zoom out
+        </button>
+      </div>
+      <div className="map">
         <GoogleMap
-          mapContainerStyle={{ width: "100%", height: "800px" }}
-          center={initialCenter}
+          mapContainerClassName="map-container"
+          center={currAgency}
           zoom={initialZoom}
           options={options}
           onLoad={onLoad}
         >
           <Marker
-            position={{ lat: initialCenter.lat, lng: initialCenter.lng }}
+            position={{ lat: currAgency.lat, lng: currAgency.lng }}
             icon={{
               url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
             }}
@@ -139,29 +177,13 @@ const Map = ({ agencies, initialCenter, initialZoom }) => {
             }
           </MarkerClusterer>
 
-          <Circle
-            center={initialCenter}
-            radius={15000}
-            options={closeOptions}
-          />
-          <Circle
-            center={initialCenter}
-            radius={30000}
-            options={middleOptions}
-          />
-          <Circle center={initialCenter} radius={45000} options={farOptions} />
+          <Circle center={currAgency} radius={15000} options={closeOptions} />
+          <Circle center={currAgency} radius={30000} options={middleOptions} />
+          <Circle center={currAgency} radius={45000} options={farOptions} />
         </GoogleMap>
-      {/* </LoadScript> */}
+      </div>
 
-     
       <br />
-    
-      {/* {selectedAgency && (
-        <p>Distance from {initialCenter.name} to {selectedAgency.name}: {distance} </p>
-      )}
-      {selectedAgency && (
-        <p>Duration : {duration} </p>
-      )} */}
     </div>
   );
 };
@@ -194,4 +216,5 @@ const farOptions = {
   strokeColor: "#FF5252",
   fillColor: "#FF5252",
 };
+
 export default Map;
